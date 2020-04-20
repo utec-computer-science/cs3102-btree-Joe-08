c2ad1f41305179afa3027c778abd5527b8f88ea3
Joe-08 <joe_andre0710@hotmail.com>
2020-04-20 10:18:19 -0500

    feat(insert): add insert function
    Add insert function in BTree class

--- btree.cpp
+++ btree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector> 
+#include <cmath>
 
 template <typename T>
 class SS_Traits{
@@ -56,7 +57,7 @@ class BNode {
 public:
   typedef typename T::value_t value_t;
   typedef typename T::container_t container_t;
-  typedef typename T::iterador_t iterator_t;
+  typedef typename T::iterator_t iterator_t;
   typedef std::vector< BNode<T,S>* > pcontainer_t;
 
   container_t keys;
@@ -72,6 +73,56 @@ public:
   }
 
   ~BNode(void){}
+  
+  void insertInNode(value_t val) {
+    int i = n - 1;
+
+    if (leaf) {
+      while (i >= 0 && keys[i] > val) {
+        keys[i+1] = keys[i];
+        i--;
+      }
+
+      keys[i+1] = val;
+      n++;
+    } else {
+      while (i >= 0 && keys[i] > val) i--;
+
+      if (ptrs[i+1]->n == order - 1) {
+        splitNode(i+1, ptrs[i+1]);
+        if (keys[i+1] < val) i++;
+      }
+      ptrs[i+1]->insertInNode(val);
+    }
+  }
+
+  void splitNode(int i, BNode<T,S>* node) {
+    BNode<T,S>* newNode = new BNode<T,S>();
+    newNode->leaf = node->leaf;
+    newNode->n = std::ceil(order / 2) - 1;
+
+    for (int j = 0; j < std::ceil(order / 2) - 1; j++) {
+      newNode->keys[j] = node->keys[j+newNode->n+1];
+      node->keys[j+newNode->n+1] = 0;
+    }
+
+    if (node->leaf == false) {
+      for (int j = 0; j < std::ceil(order / 2); j++)
+        newNode->ptrs[j] = node->ptrs[j+newNode->n];
+    }
+
+    node->n = std::ceil(order / 2) - 1;
+
+    ptrs[i+1] = newNode;
+
+    for (int j = n - 1; j >= i; j--)
+      keys[j+1] = keys[j];
+
+    keys[i] = node->keys[std::ceil(order/2)-1];
+    node->keys[std::ceil(order/2)-1] = 0;
+
+    n++;
+  }
 };
 
 template <typename T, int S>
@@ -93,6 +144,25 @@ public:
 
   void insert(const value_t& val = 0){
     // TODO :: INSERT
+    if (root == nullptr) {
+      root = new BNode<T,S>(); 
+      root->keys[0] = val;
+      root->n = 1;
+      root->leaf = true;
+    } else {
+      if (root->n == root->order - 1) {
+        BNode<T,S> *ptr = new BNode<T,S>();
+        ptr->ptrs[0] = root;
+        ptr->splitNode(0, root); 
+        
+        int i = 0;
+        if (ptr->keys[0] < val) i++;
+        ptr->ptrs[i]->insertInNode(val);
+
+        root = ptr;
+      } else
+        root->insertInNode(val);
+    }
   }
 
   bool find(const value_t = 0) const{
@@ -116,7 +186,8 @@ int main() {
   std::cout<<tree<< std::endl;
 
   typedef SS_Traits<float> strait_t;
-  BTree<strait_t,10> stree; 
+  BTree<strait_t,4> stree; 
+  stree.insert(1);
   std::cout<<stree<< std::endl;
 }
 
